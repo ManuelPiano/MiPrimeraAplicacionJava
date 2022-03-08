94372aa7c5389ba876581c979f7d54a77162f624
ManuelPiano <manuel.alvarado21@itca.edu.sv>
2022-03-08 11:35:27 -0600

    modiifcacion notas evaluaciones

--- src/Ejerciciosguia1/newpackage/notasEvaluaciones.java
+++ src/Ejerciciosguia1/newpackage/notasEvaluaciones.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Ejerciciosguia1.newpackage;
 
-/**
- *
- * @author ITCA
- */
+import java.text.DecimalFormat;
+
+
 public class notasEvaluaciones {
-    
+    public static void main(String[] args) {
+        double parcial1, parcial2, parcial3, parcial4, parcial5, suparcial;
+        final double eva1 = 0.25, eva2 = 0.15, eva3 = 0.15, eva4 = 0.20, eva5 = 0.25;
+        
+        parcial1 = 8.0;
+        parcial2 = 7.5;
+        parcial3 = 9.0;
+        parcial4 = 8.0;
+        parcial5 = 9.0;
+        DecimalFormat formato = new DecimalFormat("#0.00");
+        suparcial = ((parcial1*eva1)+(parcial2*eva2)+(parcial3*eva3)+(parcial4*eva4)+(parcial5*eva5));
+        System.out.println("promedio de estudiante es: " + formato.format(suparcial));
+    }
 }
